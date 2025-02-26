d1294977d9bc7baae97463927fc00f4c98c08dcd
pmangu666 <peggyonpetty1006@gmail.com>
2025-02-26 17:21:40 -0600

    trocr feature extraction

--- preprocessing/20 subjects/printcolumns.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Load the CSV file (ensure the file path is correct)
-df = pd.read_csv('C:/Users/16154/vu/Group Project/project 4/Handwriting_CNN/Source_Data/CSAFE_Handwriting_Info.csv')
-
-# Print column names
-print(df.columns)
